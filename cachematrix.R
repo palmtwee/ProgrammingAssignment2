a2823e1cf347f2683ac6039a96cbcc1323d6b97d
palmtwee <tu.yen.chu@gmail.com>
2019-04-03 06:34:47 -0400

    Add files via upload
    Added cachematrix.R for R Programming Week 3 Assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first makeCacheMatrix function takes a matrix array and outputs a
+## custom object (list) that includes the matrix and it's inverse that can 
+## be inputted into the cacheSolve function.
+## The cacheSolve function checks if the inverse of the matrix has been
+## cached. If not, it recalculates the inverse of the original matrix.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix takes an input of a matrix array and returns a list
+## of the matrix and the inverse of the matrix, cached. It also includes a
+## backup of the matrix, assuming makeCacheMatrix$matrix can be edited.
+## This allows the backup (assumed not to be edited) to serve as a checking
+## mechanism to verify if the inverse needs to be recalculated.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        mat <- x
+        inv <- solve(x)
+        backup <- mat
+        matt <- list(matrix = mat, inverse = inv, backup = backup)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes the output of makeCacheMatrix and returns the inverse
+## of the given matrix. If the x$matrix and x$backup are identical and 
+## x$inverse is not null, then it retrieves the cached inverse. If x$inverse
+## is null or if x$matrix has been altered to differ from x$backup, the
+## function recalculates the inverse of the matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$inverse
+        if(is.null(inv) || x$matrix != x$backup){
+                print("no cached inverse, calculating inverse...")
+                inverse <- solve(x$matrix)
+                print(inverse)
+        }else if(!is.null(inv) && x$matrix == x$backup){
+                print("retrieving cached inverse")
+                inverse <- inv
+                print(inverse)
+        }
 }
